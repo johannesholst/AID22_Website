82880f2d31ace0be3a974689071adaab133eb853
johannesholst <johannes.holst1@gmail.com>
2022-10-11 15:40:01 +0200

    Removed unused code in Javascript

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function newQuote(){
 }
 
 btn.addEventListener('click', function onClick() {
-  btn.style.backgroundColor = 'purple';
+  btn.style.backgroundColor = 'salmon';
   btn.style.color = 'white';
-
+  
 });
